a23e7012208536ed9c98a5ddbe9024baaf618878
imurillus <4948877+discite@users.noreply.github.com>
2023-05-06 18:53:27 -0500

    latest changes to the library

--- crates/core/main.rs
+++ crates/core/main.rs
@@ -1,11 +1,10 @@
 use digitalization::{to_binary, to_decimal};
 
-
 fn main() {
     // Test the library digitalization
-    let number:u32 = 1234567890;
+    let number: u32 = 1234567890;
     println!("Decimal: {}", number);
-    let binary = to_binary(number);
+    let binary: [u8; 32] = to_binary(number);
     println!("Binary: {:?}", binary);
     let decimal_recovery = to_decimal(binary);
     println!("Decimal recovery: {}", decimal_recovery.unwrap());
